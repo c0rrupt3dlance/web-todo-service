f5d4b015a496f9756c4a9e571a914aa23ae4b290
letamm <letamm.sob@gmail.com>
2025-06-23 21:49:05 +0500

    feature: added user sign-up

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -8,26 +8,29 @@ import (
 )
 
 func (h *Handler) SignUp(c *gin.Context) {
-	var inputUser models.User
+	var (
+		inputUser models.User
+		id        int
+	)
 
 	if err := c.ShouldBindJSON(&inputUser); err != nil {
-		log.Printf("got this error as response: %s", err)
+		log.Println("user sent incorrect input data, unable to bind")
 		c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"message": "bad request",
+			"message": "incorrect data in input",
 		})
+		return
 	}
 
 	id, err := h.services.CreateUser(inputUser)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message": "internal server error",
+			"message": "something went wrong",
 		})
+		return
 	}
 
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"user_id": id,
-	})
+	c.JSON(http.StatusOK, gin.H{"user": id})
 }
 
 func (h *Handler) SignIn(c *gin.Context) {
